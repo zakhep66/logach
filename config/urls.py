6a7668c8c83a1c83a6ce8e436e2509eb1bc47122
zakhep66 <sur.mikhael@gmail.com>
2021-11-23 15:41:08 +0300

    go

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from interface.admin import manager_site
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     # path('agent/', manager_site.urls),
     # path('accountant/', manager_site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
