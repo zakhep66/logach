70581a8ea0f8b4165e1c1bce988ae4d1bbcb2604
zakhep66 <sur.mikhael@gmail.com>
2021-11-25 13:13:11 +0300

    ыва

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m01m=)yd1wcbbv^hnml-i852l5+vuhhv3v&skhfs9awbyafozn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
